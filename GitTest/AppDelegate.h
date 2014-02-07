84a159cd64d3b563178a5be949bb9aef57732ea8
Erik Bobbink <bobbinke@gmail.com>
2014-02-07 18:20:54 +0100

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by erik bobbink on 07/02/2014.
+//  Copyright (c) 2014 Erik Bobbink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by erik bobbink on 07/02/2014.
+//  Copyright (c) 2014 Erik Bobbink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
