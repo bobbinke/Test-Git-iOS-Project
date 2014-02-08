91ab64892c6c726f9af5cd776ec73fc1c3afad55
Erik Bobbink <bobbinke@gmail.com>
2014-02-08 09:33:29 +0100

    added test class

---
+++ GitTest/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by erik bobbink on 08/02/2014.
+//  Copyright (c) 2014 Erik Bobbink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ GitTest/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by erik bobbink on 08/02/2014.
+//  Copyright (c) 2014 Erik Bobbink. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
